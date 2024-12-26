097b7827f8b34a9972932917af85e594c467cb96
Sayedur Rahman <sayedur22@yahoo.com>
2024-12-26 12:23:34 -0800

    modified

--- src/Java_Practise3/Inhartance/Parents_clss.java
+++
@@ -1,5 +0,0 @@
-package Java_Practise3.Inhartance;
-
-public class Parents_clss {
-    int p =10;
-}
--- src/Java_Practise3/Inhartance/Sub_Cls.java
+++
@@ -1,9 +0,0 @@
-package Java_Practise3.Inhartance;
-
-public class Sub_Cls extends Parrent_clss {
-    public static void main(String[] args) {
-        Sub_Cls sb=new Sub_Cls();
-        sb.Multiplication(10,20);
-        System.out.println(("parent class value showing in subclass:"+ sb.result));
-    }
-}
